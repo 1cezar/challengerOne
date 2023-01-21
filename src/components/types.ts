393c5e5e7482c4930c876ac7c4b123dccfdf3f2d
1cezar <cezar.rocah@skalena.com>
2023-01-21 18:23:47 -0300

    components and validations

---
+++ src/components/types.ts
@@ -0,0 +1,5 @@
+/**
+ * @tutorial https://www.typescriptlang.org/pt/docs/handbook/manipulacao-dom.html
+ */
+
+export type InputsElements = HTMLSelectElement | HTMLInputElement | HTMLTextAreaElement;
